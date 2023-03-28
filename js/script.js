7196a1302b83704ba6e5c7f7c89e1ee397286297
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-03-29 01:00:33 +0200

    Job completed

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const {createApp} = Vue;
+
+createApp({
+    data(){
+        return {
+            message: `Hello World, I'm Mirko Sorrentino. Of course this is not me, but he is my hero`,
+            imgSrc: "img/MJ.jpg"
+        }
+    },
+    methods: {
+
+    }
+}).mount("#app")
